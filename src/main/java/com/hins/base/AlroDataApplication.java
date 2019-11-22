509a063112cf09ccaef6a495b7a8c019323fbdcf
wyman <277439189@qq.com>
2019-11-22 09:31:00 +0800

    first commit

---
+++ src/main/java/com/hins/base/AlroDataApplication.java
@@ -0,0 +1,13 @@
+package com.hins.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlroDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlroDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hins/base/AlroDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hins.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlroDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
